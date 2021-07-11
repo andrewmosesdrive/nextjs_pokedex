14c2827dfe05b2a794cea3aaed03710914a06b99
Andrew Moses <andrewmosesdrive@gmail.com>
2021-07-11 16:07:21 -0700

    pokeapi data coming through, fetch implemented

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-
+import useFetch from "react-fetch-hook";;
 
 export default function Home() {
+
+  const { isLoading, error, data } = useFetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151');
+  console.log(data)
+  if (isLoading) return "Loading...";
+  if (error) return "Error!";
+
   return (
     <div>
       yeet
