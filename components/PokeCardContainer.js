9c0a05bcf12e15ae18997b9416086ab2811ad4f2
daneburns <daneb91@gmail.com>
2021-07-18 21:56:36 -0700

    refactored for based graphQL

--- components/PokeCardContainer.js
+++ components/PokeCardContainer.js
@@ -1,24 +1,65 @@
 import useFetch from "react-fetch-hook";
+import { useEffect } from "react";
 import CardBody from "./CardBody";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_POKEMANS = gql`
+  query samplePokeAPIquery {
+    pokemon_v2_pokemon(order_by: { id: asc }) {
+      id
+      name
+      pokemon_v2_pokemontypes {
+        type_id
+      }
+    }
+  }
+`;
+
+const types = [
+  "normal",
+  "fighting",
+  "flying",
+  "poison",
+  "ground",
+  "rock",
+  "bug",
+  "ghost",
+  "steel",
+  "fire",
+  "water",
+  "grass",
+  "electric",
+  "psychic",
+  "ice",
+  "dragon",
+  "dark",
+  "fairy",
+  "unknown",
+  "shadow",
+];
 
 function PokeCardDesktop() {
-  const { isLoading, error, data } = useFetch(
-    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"
-  );
-  console.log(data);
-
-  if (isLoading) return "Loading...";
-  if (error) return "Error!";
+  const { loading, error, data } = useQuery(GET_POKEMANS);
 
   return (
     <div className="grid grid-cols-3 space-y-4 space-x-4 max-w-6xl justify-center items-center mx-auto">
-      {data.results.map((value, index) => {
-        return (
-          <>
-            <CardBody index={index} value={value} />
-          </>
-        );
-      })}
+      {data &&
+        data.pokemon_v2_pokemon.map((pokemon) => {
+          let typesArray =
+            pokemon.pokemon_v2_pokemontypes.map(
+              (pokemonType) => types[pokemonType.type_id - 1]
+            ) || "";
+
+          return (
+            <>
+              <CardBody
+                id={pokemon.id}
+                name={pokemon.name}
+                types={typesArray}
+              />
+            </>
+          );
+        })}
     </div>
   );
 }
---
+++ components/TypeTag.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TypeTag({ type }) {
+  return <div className="px-4 py-2 rounded-lg bg-red-200 mx-1">{type}</div>;
+}
